46fe630660f1cee2fe5fe86955e8e7998c6695df
shivam219 <shivamchoudhary2002119@gmail.com>
2022-03-15 12:08:27 +0530

    main

--- DSA/src/exce/lol.java
+++
@@ -1,8 +0,0 @@
-package exce;
-public class lol {
-		public static void main(String[] args) {
-			me ob = new me();
-//			throw new Exception("shivam");
-			throw new ArithmeticException("shivam");
-		}
-}
